69558604ad7c9d6cb1e0fbc2ad7a89cf4f8016ec
«SanFili» <filimonova.alexandra@yandex.ru>
2020-05-06 19:06:36 +0300

    верстка блока cover

--- components/Cover.vue
+++ components/Cover.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    components/Cover
+  <div class="cover">
+    <p class="cover__tag">#РАКЛЕЧИТСЯ</p>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.cover {
+  background: #613a93;
+  width: 100%;
+  height: 689px;
+  margin: 0;
+}
+</style>
