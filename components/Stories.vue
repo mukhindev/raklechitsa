90c474460cb2527593551be7b72b173391ba610c
mukhindev <mukhin.dev@gmail.com>
2020-05-07 23:41:59 +0300

    feat: в Stories/_id фото, имя и цитата, текст

--- components/Stories.vue
+++ components/Stories.vue
@@ -1,34 +1,24 @@
 <template>
   <section class="stories">
-    <section-heading>
+    <section-heading class="stories__heading">
       <template #title>Истории неизлечимых привычек</template>
       <template #subtitle
         >А вот здесь выводится подзаголовок. Это временно, в данной секции он не
         нужен, не забыть убрать</template
       >
     </section-heading>
-    <ul class="stories__grid">
-      <story-card v-for="story in stories" :key="story.id" :story="story" />
-    </ul>
-    <nuxt-link to="/stories" class="stories__button-more">
-      <span class="stories__button-text">Больше статей</span>
-    </nuxt-link>
+    <stories-grid start="0" limit="8" more />
   </section>
 </template>
 
 <script>
 import SectionHeading from './SectionHeading.vue';
-import StoryCard from './StoryCard.vue';
+import StoriesGrid from './StoriesGrid.vue';
 
 export default {
   components: {
     SectionHeading,
-    StoryCard,
-  },
-  computed: {
-    stories() {
-      return this.$store.getters['stories/getStories'](0, 8);
-    },
+    StoriesGrid,
   },
 };
 </script>
@@ -37,32 +27,7 @@ export default {
 .stories {
   padding: 50px 60px;
 }
-.stories__grid {
-  list-style: none;
-  padding: 0;
-  margin: 70px 0;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 40px;
-}
-.stories__button-more {
-  padding: 31px 0;
-  background-color: #fbfbfb;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: 0.25s;
-  cursor: pointer;
-  text-decoration: none;
-}
-.stories__button-more:hover {
-  color: #fff;
-  background-color: #613a93;
-}
-.stories__button-text {
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 1.25;
+.stories__heading {
+  margin: 0 0 70px;
 }
 </style>
