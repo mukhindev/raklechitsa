5e17138629a0f003d4a22e8201f6b73098cd92a8
mukhindev <mukhin.dev@gmail.com>
2020-05-11 17:23:46 +0300

    fix: About

--- components/blocks/About.vue
+++ components/blocks/About.vue
@@ -1,40 +1,56 @@
 <template>
   <section class="about">
-    <ui-container class="stats__container">
-      <div class="about__container">
-        <h3 class="about__title">#РАКЛЕЧИТСЯ</h3>
-        <div class="about__text">
-          <div class="about__one-colum">
-            <h4 class="about__one-colum-title">О проекте</h4>
-            <p class="about__one-colum-subtitle">
-              Этот&nbsp;проект&nbsp;был&nbsp;создан
-              благотворительным&nbsp;фондом Константина&nbsp;Хабенского.
-            </p>
-          </div>
-          <div class="about__two-colums">
-            <div class="about__two-colums-brif">
-              <h5 class="about__two-colums-brif-title">Рак&nbsp;Лечится</h5>
-              <p class="about__two-colums-brif-subtitle">
-                Фонд&nbsp;Хабенского
-              </p>
-            </div>
-            <div class="about__two-colums-text">
-              <p class="about__two-colums-paragraph">
-                Есть вещи, которые не лечатся. Особенности характера, страстные
-                увлечения, привычки, ставшие частью нашего «я», фобии, которые
-                мы приобрели в детстве. Список можно продолжать до
-                бесконечности, но одна болезнь в него точно не войдет. Эта
-                болезнь — рак. Рак лечится, и лучшее доказательство — люди с их
-                неизлечимыми особенностями, которые сумели победить рак.
-              </p>
-              <p class="about__two-colums-paragraph">
-                Рак лечится — проект Благотворительного Фонда Константина
-                Хабенского и Leo Burnett Moscow. С его помощью мы надеемся
-                изменить отношение людей к раку и заставить каждого поверить:
-                онкологическое заболевание — это не приговор.
-              </p>
-            </div>
-          </div>
+    <ui-container class="about__container">
+      <h3 class="about__tag">#РАКЛЕЧИТСЯ</h3>
+      <div class="about__grid">
+        <ui-heading class="stats__heading" theme="dark">
+          <template #title>О проекте</template>
+          <template #subtitle
+            >Этот проект был создан благотворительным фондом Константина
+            Хабенского.</template
+          >
+        </ui-heading>
+        <div class="about__menu">
+          <p
+            class="about__item"
+            :class="{ about__item_active: tab === 1 }"
+            @click="tab = 1"
+          >
+            Рак Лечится
+          </p>
+          <p
+            class="about__item"
+            :class="{ about__item_active: tab === 2 }"
+            @click="tab = 2"
+          >
+            Фонд Хабенского
+          </p>
+        </div>
+        <div v-if="tab === 1" class="about__text">
+          <p class="about__paragraph">
+            Есть вещи, которые не лечатся. Особенности характера, страстные
+            увлечения, привычки, ставшие частью нашего «я», фобии, которые мы
+            приобрели в детстве. Список можно продолжать до бесконечности, но
+            одна болезнь в него точно не войдет. Эта болезнь — рак. Рак лечится,
+            и лучшее доказательство — люди с их неизлечимыми особенностями,
+            которые сумели победить рак.
+          </p>
+          <p class="about__paragraph">
+            Рак лечится — проект Благотворительного Фонда Константина Хабенского
+            и Leo Burnett Moscow. С его помощью мы надеемся изменить отношение
+            людей к раку и заставить каждого поверить: онкологическое
+            заболевание — это не приговор.
+          </p>
+        </div>
+        <div v-if="tab === 2" class="about__text">
+          <p class="about__paragraph">
+            Благотворительный Фонд Константина Хабенского с 2008 года помогает
+            детям с онкологическими и другими тяжелыми заболеваниями головного
+            мозга. Фонд не только поддерживает семью заболевшего ребенка в самый
+            сложный момент, оплачивая обследования, лечение и медицинские
+            препараты, но и в целом меняет систему оказания помощи детям с
+            опухолями мозга в России.
+          </p>
         </div>
       </div>
     </ui-container>
@@ -43,10 +59,17 @@
 
 <script>
 import Container from '~/components/ui/Container';
+import Heading from '~/components/ui/Heading';
 
 export default {
+  data() {
+    return {
+      tab: 1,
+    };
+  },
   components: {
     'ui-container': Container,
+    'ui-heading': Heading,
   },
 };
 </script>
@@ -57,116 +80,87 @@ export default {
   color: #ffffff;
 }
 
-.about__container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 100px;
-  padding-bottom: 100px;
-}
-
-.about__title {
-  max-width: 720px;
-  margin: 0;
-  margin-bottom: 70px;
+.about__tag {
+  margin: 0 0 70px;
   font-weight: 800;
   font-size: 64px;
-  line-height: 77px;
+  line-height: 1.25;
   text-align: center;
 }
 
+.about__container {
+  padding-top: 90px;
+  padding-bottom: 100px;
+}
+
+.about__grid {
+  display: grid;
+  grid-template-columns: auto auto 640px;
+}
+
 @media screen and (max-width: 1280px) {
-  .about__title {
-    margin-bottom: 60px;
+  .about__grid {
+    grid-template-columns: auto auto 570px;
   }
 }
 
-.about__text {
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  padding: 0;
-  margin: 0;
+@media screen and (max-width: 1024px) {
+  .about__grid {
+    grid-template-columns: auto auto 447px;
+  }
 }
 
-.about__one-colum {
-  margin: 0;
-  padding: 0;
-  max-width: 340px;
+.about__menu {
+  justify-self: right;
+  margin-right: 40px;
 }
 
-.about__one-colum-title {
-  margin: 0;
-  padding: 0;
-  padding-bottom: 32px;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 32px;
-  line-height: 36px;
+@media screen and (max-width: 1024px) {
+  .about__menu {
+    margin-right: 30px;
+  }
 }
 
-.about__one-colum-subtitle {
-  margin: 0;
-  padding: 0;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  color: #dedede;
-}
-
-.about__two-colums {
-  display: flex;
-  justify-content: space-between;
-  margin: 0;
-  padding: 0;
-  padding-top: 68px;
-}
-
-.about__two-colums-text {
-  max-width: 640px;
-}
-
-.about__two-colums-brif {
-  padding: 0;
-  padding-right: 40px;
-}
-
-.about__two-colums-brif-title {
-  margin: 0;
-  padding: 0;
-  padding-bottom: 10px;
-  text-align: end;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 22px;
-}
-
-.about__two-colums-brif-subtitle {
-  margin: 0;
-  padding: 0;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 22px;
-  text-align: end;
+.about__item {
+  margin: 0 0 10px;
+  text-align: right;
   color: #c9c9c9;
+  cursor: pointer;
+  white-space: nowrap;
+  font-size: 18px;
+  line-height: 1.25;
 }
 
-.about__two-colums-paragraph {
-  margin: 0;
-  padding: 0;
-  max-width: 640px;
-  font-family: Inter;
+@media screen and (max-width: 1024px) {
+  .about__item {
+    margin: 0 0 8px;
+    font-size: 15px;
+  }
+}
+
+.about__item_active {
+  margin: 0 0 10px;
+  text-align: right;
+  color: #ffffff;
+  cursor: default;
+}
+
+.about__paragraph {
+  margin: 0 0 20px;
   font-style: normal;
   font-weight: normal;
   font-size: 18px;
-  line-height: 22px;
+  line-height: 1.25;
   color: #dedede;
 }
-.about__two-colums-paragraph:first-of-type {
-  padding-bottom: 20px;
+
+@media screen and (max-width: 1024px) {
+  .about__paragraph {
+    font-size: 15px;
+  }
+}
+
+.about__paragraph:last-of-type {
+  margin: 0;
 }
 </style>
