0048eabd83503e79e22e2337076048f4bdfa4b7e
mukhindev <mukhin.dev@gmail.com>
2020-05-15 17:42:05 +0300

    hotfix: блок About с адоптивностью, компонетн Tabs,

--- components/blocks/About.vue
+++ components/blocks/About.vue
@@ -2,56 +2,15 @@
   <section class="about">
     <ui-container class="about__container">
       <h3 class="about__tag">#РАКЛЕЧИТСЯ</h3>
-      <div class="about__grid">
-        <ui-heading class="stats__heading" theme="dark">
+      <div class="about__colunms">
+        <ui-heading class="about__heading" theme="dark">
           <template #title>О проекте</template>
           <template #subtitle
             >Этот проект был создан благотворительным фондом Константина
             Хабенского.</template
           >
         </ui-heading>
-        <div class="about__menu">
-          <p
-            class="about__item"
-            :class="{ about__item_active: tab === 1 }"
-            @click="tab = 1"
-          >
-            Рак Лечится
-          </p>
-          <p
-            class="about__item"
-            :class="{ about__item_active: tab === 2 }"
-            @click="tab = 2"
-          >
-            Фонд Хабенского
-          </p>
-        </div>
-        <div v-if="tab === 1" class="about__text">
-          <p class="about__paragraph">
-            Есть вещи, которые не лечатся. Особенности характера, страстные
-            увлечения, привычки, ставшие частью нашего «я», фобии, которые мы
-            приобрели в детстве. Список можно продолжать до бесконечности, но
-            одна болезнь в него точно не войдет. Эта болезнь — рак. Рак лечится,
-            и лучшее доказательство — люди с их неизлечимыми особенностями,
-            которые сумели победить рак.
-          </p>
-          <p class="about__paragraph">
-            Рак лечится — проект Благотворительного Фонда Константина Хабенского
-            и Leo Burnett Moscow. С его помощью мы надеемся изменить отношение
-            людей к раку и заставить каждого поверить: онкологическое
-            заболевание — это не приговор.
-          </p>
-        </div>
-        <div v-if="tab === 2" class="about__text">
-          <p class="about__paragraph">
-            Благотворительный Фонд Константина Хабенского с 2008 года помогает
-            детям с онкологическими и другими тяжелыми заболеваниями головного
-            мозга. Фонд не только поддерживает семью заболевшего ребенка в самый
-            сложный момент, оплачивая обследования, лечение и медицинские
-            препараты, но и в целом меняет систему оказания помощи детям с
-            опухолями мозга в России.
-          </p>
-        </div>
+        <ui-tabs :tabs="tabs" theme="dark" />
       </div>
     </ui-container>
   </section>
@@ -60,16 +19,32 @@
 <script>
 import Container from '~/components/ui/Container';
 import Heading from '~/components/ui/Heading';
+import Tabs from '~/components/ui/Tabs';
 
 export default {
-  data() {
-    return {
-      tab: 1,
-    };
-  },
   components: {
     'ui-container': Container,
     'ui-heading': Heading,
+    'ui-tabs': Tabs,
+  },
+  data() {
+    return {
+      tabs: [
+        {
+          name: 'Рак Лечится',
+          content: [
+            'Есть вещи, которые не лечатся. Особенности характера, страстные увлечения, привычки, ставшие частью нашего «я», фобии, которые мы приобрели в детстве. Список можно продолжать до бесконечности, но одна болезнь в него точно не войдет. Эта болезнь — рак. Рак лечится, и лучшее доказательство — люди с их неизлечимыми особенностями, которые сумели победить рак.',
+            'Рак лечится — проект Благотворительного Фонда Константина Хабенского и Leo Burnett Moscow. С его помощью мы надеемся изменить отношение людей к раку и заставить каждого поверить: онкологическое заболевание — это не приговор.',
+          ],
+        },
+        {
+          name: 'Фонд Хабенского',
+          content: [
+            'Благотворительный Фонд Константина Хабенского с 2008 года помогает детям с онкологическими и другими тяжелыми заболеваниями головного мозга. Фонд не только поддерживает семью заболевшего ребенка в самый сложный момент, оплачивая обследования, лечение и медицинские препараты, но и в целом меняет систему оказания помощи детям с опухолями мозга в России.',
+          ],
+        },
+      ],
+    };
   },
 };
 </script>
@@ -80,6 +55,23 @@ export default {
   color: #ffffff;
 }
 
+.about__container {
+  padding-top: 90px;
+  padding-bottom: 100px;
+}
+
+@media screen and (max-width: 768px) {
+  .about__container {
+    padding: 80px 40px 80px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .about__container {
+    padding: 50px 15px 50px;
+  }
+}
+
 .about__tag {
   margin: 0 0 70px;
   font-weight: 800;
@@ -88,79 +80,29 @@ export default {
   text-align: center;
 }
 
-.about__container {
-  padding-top: 90px;
-  padding-bottom: 100px;
-}
-
-.about__grid {
-  display: grid;
-  grid-template-columns: auto auto 640px;
-}
-
-@media screen and (max-width: 1280px) {
-  .about__grid {
-    grid-template-columns: auto auto 570px;
+@media screen and (max-width: 768px) {
+  .about__tag {
+    display: none;
   }
 }
 
-@media screen and (max-width: 1024px) {
-  .about__grid {
-    grid-template-columns: auto auto 447px;
+.about__colunms {
+  display: flex;
+}
+
+@media screen and (max-width: 768px) {
+  .about__colunms {
+    flex-direction: column;
+    align-items: center;
+  }
+  .about__heading {
+    margin: 0 0 80px;
   }
 }
 
-.about__menu {
-  justify-self: right;
-  margin-right: 40px;
-}
-
-@media screen and (max-width: 1024px) {
-  .about__menu {
-    margin-right: 30px;
+@media screen and (max-width: 425px) {
+  .about__heading {
+    margin: 0 0 40px;
   }
-}
-
-.about__item {
-  margin: 0 0 10px;
-  text-align: right;
-  color: #c9c9c9;
-  cursor: pointer;
-  white-space: nowrap;
-  font-size: 18px;
-  line-height: 1.25;
-}
-
-@media screen and (max-width: 1024px) {
-  .about__item {
-    margin: 0 0 8px;
-    font-size: 15px;
-  }
-}
-
-.about__item_active {
-  margin: 0 0 10px;
-  text-align: right;
-  color: #ffffff;
-  cursor: default;
-}
-
-.about__paragraph {
-  margin: 0 0 20px;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 1.25;
-  color: #dedede;
-}
-
-@media screen and (max-width: 1024px) {
-  .about__paragraph {
-    font-size: 15px;
-  }
-}
-
-.about__paragraph:last-of-type {
-  margin: 0;
 }
 </style>
---
+++ components/ui/Tabs.vue
@@ -0,0 +1,238 @@
+<template>
+  <div class="tabs">
+    <nav class="tabs__nav">
+      <ul class="tabs__nav-tabs">
+        <li class="tabs__nav-item" v-for="(tab, index) in tabs" :key="tab.name">
+          <button
+            @click="activeTab = index"
+            class="tabs__nav-button"
+            :class="[
+              mod('tabs__nav-button_theme_'),
+              { 'tabs__nav-button_active': activeTab === index },
+            ]"
+          >
+            {{ tab.name }}
+          </button>
+        </li>
+      </ul>
+    </nav>
+    <div class="tabs__tab-content">
+      <p
+        v-for="paragraph in tabs[activeTab].content"
+        :key="paragraph"
+        class="tabs__tab-paragraph"
+        :class="mod('tabs__tab-paragraph_theme_')"
+      >
+        {{ paragraph }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tabs: {
+      type: Array,
+      default: [],
+    },
+    theme: String,
+  },
+  data() {
+    return {
+      activeTab: 0,
+    };
+  },
+  computed: {
+    mod() {
+      return function(m) {
+        return [this.theme ? `${m}${this.theme}` : ''];
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tabs {
+  display: flex;
+  justify-content: flex-end;
+}
+
+@media screen and (max-width: 768px) {
+  .tabs {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+}
+
+.tabs__nav-tabs {
+  flex-shrink: 0;
+  list-style: none;
+  margin: 0 40px;
+  padding: 0;
+}
+
+@media screen and (max-width: 1024px) {
+  .tabs__nav-tabs {
+    margin: 0 30px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .tabs__nav-tabs {
+    margin: 0 0 24px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .tabs__nav-tabs {
+    margin: 0 0 16px;
+  }
+}
+
+.tabs__nav-item {
+  margin: 0 0 10px;
+  text-align: right;
+}
+
+@media screen and (max-width: 1024px) {
+  .tabs__nav-item {
+    margin: 0 0 8px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .tabs__nav-item {
+    display: inline;
+    text-align: left;
+    margin: 0 30px 0 0;
+  }
+}
+@media screen and (max-width: 425px) {
+  .tabs__nav-item {
+    margin: 0 16px 0 0;
+  }
+}
+
+.tabs__nav-item:last-of-type {
+  margin: 0;
+}
+
+.tabs__nav-button {
+  padding: 0;
+  border: none;
+  border-bottom: 1px solid transparent;
+  background: none;
+  font-family: 'Inter', Arial, sans-serif;
+  font-weight: normal;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 1.3;
+  white-space: nowrap;
+  color: #666666;
+  transition: 0.25s;
+  cursor: pointer;
+  transition: 0.25s;
+}
+
+@media screen and (max-width: 1024px) {
+  .tabs__nav-button {
+    font-size: 15px;
+  }
+}
+
+.tabs__nav-button:focus {
+  outline: none;
+}
+
+.tabs__nav-button:hover {
+  color: #000000;
+}
+
+@media screen and (max-width: 768px) {
+  .tabs__nav-button:hover {
+    border-bottom: 1px solid #000000;
+  }
+}
+
+.tabs__nav-button_theme_dark {
+  color: #c9c9c9;
+}
+
+.tabs__nav-button_theme_dark:hover {
+  color: rgb(255, 255, 255);
+}
+
+@media screen and (max-width: 768px) {
+  .tabs__nav-button_theme_dark:hover {
+    border-bottom: 1px solid #fff;
+  }
+}
+
+.tabs__nav-button.tabs__nav-button_active {
+  color: #000000;
+}
+
+@media screen and (max-width: 768px) {
+  .tabs__nav-button.tabs__nav-button_active {
+    border-bottom: 1px solid #000000;
+  }
+}
+
+.tabs__nav-button_theme_dark.tabs__nav-button_active {
+  color: #ffffff;
+}
+
+@media screen and (max-width: 768px) {
+  .tabs__nav-button_theme_dark.tabs__nav-button_active {
+    border-bottom: 1px solid #fff;
+  }
+}
+
+.tabs__tab-content {
+  max-width: 640px;
+}
+
+@media screen and (max-width: 1024px) {
+  .tabs__tab-content {
+    max-width: 640px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .tabs__tab-content {
+    max-width: 380px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .tabs__tab-content {
+    max-width: none;
+  }
+}
+
+.tabs__tab-paragraph {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 1.22;
+  color: #666666;
+  margin: 0 0 20px;
+}
+
+@media screen and (max-width: 1024px) {
+  .tabs__tab-paragraph {
+    font-size: 15px;
+    margin: 0 0 16px;
+  }
+}
+
+.tabs__tab-paragraph:last-of-type {
+  margin: 0;
+}
+
+.tabs__tab-paragraph_theme_dark {
+  color: #c9c9c9;
+}
+</style>
