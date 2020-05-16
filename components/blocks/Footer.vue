63cce01a2390c0d3c058390f70db2ee2fc63e34a
mukhindev <mukhin.dev@gmail.com>
2020-05-16 16:37:02 +0300

    refactor: адаптивность Footer

--- components/blocks/Footer.vue
+++ components/blocks/Footer.vue
@@ -1,39 +1,32 @@
 <template>
   <footer class="footer">
     <ui-container class="footer__container">
-      <div class="footer__navigation">
+      <div class="footer__columns">
         <h2 class="footer__title">
           Спасибо всем, кто помог состояться этому проекту
         </h2>
         <nav class="footer__menu">
-          <ul class="footer__links">
-            <li>
-              <nuxt-link to="/" class="footer__link">Главная</nuxt-link>
-            </li>
-            <li>
-              <nuxt-link to="/stories" class="footer__link">Истории</nuxt-link>
-            </li>
-          </ul>
+          <block-menu :quizButton="false" />
         </nav>
         <ul class="footer__social-list">
-          <li class="footer__social-link">
+          <li class="footer__social-item">
             Мы в
             <a
-              class="footer__social-away"
+              class="footer__social-link"
               href="https://www.instagram.com/raklechitsa/"
               target="_blank"
               >Инстаграме</a
             >
             и
             <a
-              class="footer__social-away"
+              class="footer__social-link"
               href="https://www.youtube.com/results?search_query=%23%D1%8D%D1%82%D0%BE%D0%BD%D0%B5%D0%BB%D0%B5%D1%87%D0%B8%D1%82%D1%81%D1%8F"
               target="_blank"
               >Youtube</a
             >
           </li>
-          <li>
-            <button class="footer__social-link" @click="shareOpen">
+          <li class="footer__social-item">
+            <button class="footer__share-button" @click="shareOpen">
               Поделитесь &#8599;
             </button>
           </li>
@@ -51,10 +44,12 @@
 
 <script>
 import Container from '~/components/ui/Container';
+import Menu from '~/components/blocks/Menu';
 
 export default {
   components: {
     'ui-container': Container,
+    'block-menu': Menu,
   },
   methods: {
     shareOpen() {
@@ -71,100 +66,244 @@ export default {
 
 <style scoped>
 .footer {
-  margin: 0 auto;
   background: #fbfbfb;
 }
 
 .footer__container {
   padding-top: 60px;
   padding-bottom: 60px;
+}
+
+@media screen and (max-width: 1280px) {
+  .footer__container {
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+}
+
+.footer__columns {
   display: flex;
-  flex-direction: column;
+}
+
+@media screen and (max-width: 720px) {
+  .footer__columns {
+    flex-direction: column;
+  }
 }
 
 .footer__title {
+  margin: 0;
+  flex-shrink: 0;
   max-width: 340px;
-  margin-right: 113px;
-  font-size: 2rem;
-  line-height: 1.1;
+  font-style: normal;
   font-weight: 600;
-  margin-block-start: 0;
-  margin-block-end: 0;
+  font-size: 32px;
+  line-height: 1.125;
 }
 
-.footer__navigation {
-  display: flex;
-  margin-bottom: 110px;
+@media screen and (max-width: 1280px) {
+  .footer__title {
+    max-width: 305px;
+    font-size: 28px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .footer__title {
+    max-width: 305px;
+    font-size: 24px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .footer__title {
+    max-width: 268px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .footer__title {
+    max-width: none;
+    font-size: 18px;
+  }
 }
 
 .footer__menu {
-  margin-right: 379px;
+  margin-left: 113px;
+  width: 100%;
 }
 
-.footer__links {
-  list-style: none;
-  display: flex;
-  padding-left: 0;
-  margin-block-start: 0;
-  margin-block-end: 0;
-}
-.footer__link {
-  margin-right: 40px;
-  text-decoration: none;
-  color: black;
-  font-size: 1.125rem;
-  line-height: 1.3;
+@media screen and (max-width: 1280px) {
+  .footer__menu {
+    margin-left: 102px;
+  }
 }
 
-ul.footer__links li:last-child a {
-  margin-right: 0;
+@media screen and (max-width: 1024px) {
+  .footer__menu {
+    margin-left: 30px;
+  }
+}
+
+@media screen and (max-width: 720px) {
+  .footer__menu {
+    margin-top: 50px;
+    margin-left: 0;
+  }
 }
 
 .footer__social-list {
+  flex-shrink: 0;
   list-style: none;
-  padding-left: 0;
-  margin-block-start: 0;
-  margin-block-end: 0;
+  margin: 0;
+  padding: 0;
+}
+
+@media screen and (max-width: 720px) {
+  .footer__social-list {
+    margin-top: 18px;
+    margin-left: 0;
+  }
+}
+
+.footer__social-item {
+  margin-bottom: 44px;
+  font-weight: normal;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 1.3;
+}
+
+@media screen and (max-width: 1280px) {
+  .footer__social-item {
+    font-size: 16px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .footer__social-item {
+    margin-bottom: 30px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .footer__social-item {
+    margin-bottom: 14px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .footer__social-item {
+    margin-bottom: 18px;
+    font-size: 13px;
+  }
+}
+
+.footer__social-item:last-of-type {
+  margin-bottom: 0;
 }
 
 .footer__social-link {
   text-decoration: none;
-  color: black;
-  font-size: 1.125rem;
-  line-height: 1.3;
-  margin-bottom: 40px;
-  display: block;
-  padding: 0;
-  border: 0;
-  cursor: pointer;
-  background-color: #fbfbfb;
+  color: #000;
+  transition: all 0.25s ease;
+  border-bottom: 1px solid #000;
 }
 
-.footer__social-away {
-  color: black;
-  font-size: 1.125rem;
+.footer__social-link:hover {
+  color: #613a93;
+  border-bottom: 1px solid #613a93;
+}
+
+.footer__share-button {
+  font-family: 'Inter', Arial, Helvetica, sans-serif;
+  padding: 0;
+  border: none;
+  background: none;
+  font-weight: normal;
+  font-style: normal;
+  font-size: 18px;
   line-height: 1.3;
+  color: #000;
+  transition: all 0.25s ease;
+  border-bottom: 1px solid transparent;
+  cursor: pointer;
+  white-space: nowrap;
+}
+
+@media screen and (max-width: 1280px) {
+  .footer__share-button {
+    font-size: 16px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .footer__share-button {
+    font-size: 13px;
+  }
+}
+
+.footer__share-button:hover {
+  color: #613a93;
+  border-bottom: 1px solid #613a93;
+}
+.footer__share-button:focus {
+  outline-style: none;
 }
 
 .footer__copyright {
+  margin: 110px 0 0;
   display: flex;
   justify-content: space-between;
+  font-size: 18px;
+  line-height: 1.3;
+}
+
+@media screen and (max-width: 1280px) {
+  .footer__copyright {
+    margin: 110px 0 0;
+    font-size: 16px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .footer__copyright {
+    margin: 90px 0 0;
+  }
+}
+
+@media screen and (max-width: 720px) {
+  .footer__copyright {
+    flex-direction: column;
+    margin: 50px 0 0;
+    font-size: 13px;
+  }
 }
 
 .footer__project-name {
-  margin-block-start: 0;
-  margin-block-end: 0;
-  font-size: 1.125rem;
-  line-height: 1;
+  margin: 0;
   color: #898989;
+  font-weight: normal;
+  font-style: normal;
+}
+
+@media screen and (max-width: 720px) {
+  .footer__project-name {
+    flex-direction: column;
+    margin: 0 0 18px;
+    font-size: 13px;
+  }
 }
 
 .footer__author {
-  margin-block-start: 0;
-  margin-block-end: 0;
-  font-size: 1.125rem;
-  line-height: 1;
   color: #898989;
   text-decoration: none;
+  transition: all 0.25s ease;
+  border-bottom: 1px solid transparent;
+}
+
+.footer__author:hover {
+  color: #613a93;
+  border-bottom: 1px solid #613a93;
 }
 </style>
---
+++ components/blocks/Menu.vue
@@ -0,0 +1,133 @@
+<template>
+  <ul class="menu__links">
+    <li class="menu__item">
+      <nuxt-link to="/" class="menu__link">Главная</nuxt-link>
+    </li>
+    <li class="menu__item">
+      <nuxt-link to="/stories" class="menu__link">Истории</nuxt-link>
+    </li>
+    <li v-if="quizButton" class="menu__item">
+      <button @click="quizOpen" class="menu__button">Рассказать историю</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    quizButton: Boolean,
+  },
+  methods: {
+    quizOpen() {
+      this.$store.commit('popup/quizOpen');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.menu__links {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+}
+
+@media screen and (max-width: 920px) {
+  .menu__links {
+    flex-direction: column;
+    margin-right: 0x;
+  }
+}
+
+.menu__item {
+  margin-right: 40px;
+}
+
+@media screen and (max-width: 1024px) {
+  .menu__item {
+    margin-right: 30px;
+    margin-bottom: 18px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .menu__item {
+    margin-right: 0px;
+    margin-bottom: 18px;
+  }
+}
+
+.menu__item:last-of-type {
+  margin: 0;
+}
+
+.menu__link {
+  font-weight: normal;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 1.3;
+  text-decoration: none;
+  color: #000;
+  transition: all 0.25s ease;
+  border-bottom: 1px solid transparent;
+}
+
+.menu__link:hover {
+  color: #613a93;
+  border-bottom: 1px solid #613a93;
+}
+
+@media screen and (max-width: 1280px) {
+  .menu__link {
+    font-size: 16px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .menu__link {
+    font-size: 13px;
+  }
+}
+
+.menu__button {
+  font-family: 'Inter', Arial, Helvetica, sans-serif;
+  padding: 0;
+  border: none;
+  background: none;
+  font-weight: normal;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 1.3;
+  color: #000;
+  transition: all 0.25s ease;
+  border-bottom: 1px solid transparent;
+  cursor: pointer;
+  white-space: nowrap;
+}
+
+@media screen and (max-width: 1280px) {
+  .menu__button {
+    font-size: 16px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .menu__button {
+    font-size: 13px;
+  }
+}
+
+.menu__button:hover {
+  color: #613a93;
+  border-bottom: 1px solid #613a93;
+}
+.menu__button:focus {
+  outline-style: none;
+}
+
+.nuxt-link-exact-active {
+  color: #000;
+  border-bottom: 1px solid #000;
+}
+</style>
