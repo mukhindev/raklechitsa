19922c2a36f2d249ece30f03f6fa0254f6f76ece
SanFili <filimonova.alexandra@yandex.ru>
2020-05-17 23:42:17 +0300

    поменяла flex на grid

--- pages/stories/_id.vue
+++ pages/stories/_id.vue
@@ -7,16 +7,16 @@
           :alt="`фото ${story.person}`"
           class="story__photo"
         />
-        <div class="story__intro">
-          <div class="story__desc">
-            <h3 class="story__person">{{ story.person }}:</h3>
-            <p class="story__quote">«{{ story.quote }}»</p>
-          </div>
-          <div class="story__misc">
-            <a href="#" class="story__sharing-link">Поделитесь &#8599;</a>
-            <p class="story__date">20 апреля 2018</p>
-          </div>
+        <!-- <div class="story__intro"> -->
+        <div class="story__desc">
+          <h3 class="story__person">{{ story.person }}:</h3>
+          <p class="story__quote">«{{ story.quote }}»</p>
         </div>
+        <div class="story__misc">
+          <a href="#" class="story__sharing-link">Поделитесь &#8599;</a>
+          <p class="story__date">20 апреля 2018</p>
+        </div>
+        <!-- </div> -->
       </div>
       <div class="story__text">
         {{ story.story }}
@@ -52,19 +52,33 @@ export default {
 
 <style scoped>
 .story {
-  padding: 100px 60px;
+  padding: 100px 20px;
 }
 .story__lead {
-  display: flex;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-areas:
+    'photo desc'
+    'photo misc';
   margin: 0 0 130px;
 }
+
 .story__photo {
-  width: 50%;
+  width: calc(100% - 60px);
   max-width: 580px;
-  margin: 0 60px 0 0;
+  margin: 0;
   object-fit: contain;
   object-position: top;
+  grid-area: photo;
 }
+
+@media screen and (max-width: 1024px) {
+  .story__photo {
+    width: calc(100% - 40px);
+  }
+}
+
 .story__intro {
   font-style: normal;
   font-weight: 500;
@@ -76,6 +90,17 @@ export default {
   justify-content: space-between;
 }
 
+.story__desc {
+  padding-top: 30px;
+  border-top: 1px solid #efefef;
+  grid-area: desc;
+}
+@media screen and (max-width: 1024px) {
+  .story__desc {
+    padding-top: 20px;
+  }
+}
+
 .story__person {
   display: inline;
   margin: 0;
@@ -83,6 +108,12 @@ export default {
   font-size: 38px;
   line-height: 1.26;
 }
+@media screen and (max-width: 1024px) {
+  .story__person {
+    font-size: 30px;
+  }
+}
+
 .story__quote {
   display: inline;
   margin: 0;
@@ -90,6 +121,12 @@ export default {
   font-size: 38px;
   line-height: 1.26;
 }
+@media screen and (max-width: 1024px) {
+  .story__quote {
+    font-size: 30px;
+  }
+}
+
 .story__text {
   max-width: 780px;
   margin: 0 auto 70px;
@@ -98,11 +135,27 @@ export default {
   font-size: 22px;
   line-height: 1.36;
 }
+@media screen and (max-width: 1023px) {
+  .story__text {
+    font-size: 18px;
+    line-height: 1.5;
+    max-width: 640px;
+    margin: 0 auto 70px;
+  }
+}
 
 .story__misc {
   display: flex;
   justify-content: space-between;
-  margin-bottom: 30px;
+  padding-bottom: 30px;
+  border-bottom: 1px solid #efefef;
+  grid-area: misc;
+  align-self: end;
+}
+@media screen and (max-width: 1024px) {
+  .story__misc {
+    padding-bottom: 20px;
+  }
 }
 
 .story__sharing-link {
@@ -114,11 +167,24 @@ export default {
   margin-block-start: 0;
   margin-block-end: 0;
 }
+@media screen and (max-width: 1023px) {
+  .story__sharing-link {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+}
+
 .story__date {
   font-size: 1.125rem;
   line-height: 1.3;
   font-weight: normal;
   margin-block-start: 0;
   margin-block-end: 0;
+}
+@media screen and (max-width: 1023px) {
+  .story__date {
+    font-size: 16px;
+    line-height: 1.5;
+  }
 }
 </style>
