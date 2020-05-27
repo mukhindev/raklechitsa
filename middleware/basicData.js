5169a45e50cad97505585ad6b22b72717693583e
mukhindev <mukhin.dev@gmail.com>
2020-05-27 23:05:03 +0300

    feat: API instagram, переиспользован Card

--- middleware/basicData.js
+++ middleware/basicData.js
@@ -2,5 +2,6 @@ export default async function({ store }) {
   await store.dispatch('blocks/fetchBlocks');
   await store.dispatch('videos/fetchVideos');
   await store.dispatch('stories/fetchStories');
+  await store.dispatch('instagram/fetchInstagram');
   await store.dispatch('statistics/fetchStatistics');
 }
--- store/instagram.js
+++ store/instagram.js
@@ -1,7 +1,20 @@
-import fakeData from '../fakeData';
+import axios from 'axios';
+
+const getPosts = data => {
+  return data.graphql.user.edge_owner_to_timeline_media.edges.map(post => {
+    const {
+      node: { display_url, accessibility_caption, shortcode },
+    } = post;
+    return {
+      img: display_url,
+      alt: accessibility_caption,
+      url: `https://instagram.com/p/${shortcode}`,
+    };
+  });
+};
 
 export const state = () => ({
-  instagram: fakeData.instagram || null,
+  instagram: [],
 });
 
 export const getters = {
@@ -11,5 +24,20 @@ export const getters = {
     } else {
       return state.instagram;
     }
+  },
+};
+
+export const mutations = {
+  setState(state, { name, data }) {
+    state[name] = data;
+  },
+};
+
+export const actions = {
+  async fetchInstagram(state) {
+    const { data } = await axios.get(
+      'https://www.instagram.com/raklechitsa/?__a=1'
+    );
+    state.commit('setState', { name: 'instagram', data: getPosts(data) });
   },
 };
