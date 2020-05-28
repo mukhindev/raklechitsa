bd0c697cd42877b421d6e2d8cb632c4ae498e6c4
mukhindev <mukhin.dev@gmail.com>
2020-05-28 23:43:38 +0300

    fix: === в fetch.js

--- middleware/fetch.js
+++ middleware/fetch.js
@@ -1,7 +1,7 @@
 export default async function({ route, store }) {
   await store.dispatch('blocks/fetchBlocks');
 
-  if (route.name == 'index') {
+  if (route.name === 'index') {
     await store.dispatch('stories/fetchStories');
     await store.dispatch('videos/fetchVideos');
     await store.dispatch('instagram/fetchInstagram');
